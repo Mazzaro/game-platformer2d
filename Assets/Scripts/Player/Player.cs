503967744eab208ee846d42504a6ee2a99a64a48
Mazzaro <mbg1@icomp.ufam.edu.br>
2026-05-21 13:59:10 -0400

    Add cinemachine and jump animations

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
+    [Header("Speed Setup")]
     public Rigidbody2D myRigidbody;
     public Vector2 velocity;
     public float speed;
@@ -11,7 +13,15 @@ public class Player : MonoBehaviour
     public float jumpForce = 2;
     public Vector2 friction = new Vector2(.1f, 0);
 
+    [Header("Animation Setup")]
+    public float jumpScaleY = 1.1f;
+     public float jumpScaleX = 1.1f;
+    public float animDuration = .3f;
+    public Ease ease = Ease.OutQuad;
+
     private float _currentSpeed;
+    private bool _isLanding;
+
 
     void Update()
     {
@@ -54,8 +64,49 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
             myRigidbody.velocity =  Vector2.up * jumpForce;
+            myRigidbody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(myRigidbody.transform);
+            HandleScaleJump();
+        }
+    }
+
+    public void HandleScaleJump()
+    {
+        myRigidbody.transform.DOScaleX(jumpScaleX, animDuration).SetLoops(2, LoopType.Yoyo);
+        myRigidbody.transform.DOScaleY(0.8f, animDuration).SetLoops(2, LoopType.Yoyo);
+        myRigidbody.transform.DOScaleY(jumpScaleY, animDuration).SetLoops(2, LoopType.Yoyo);
+        myRigidbody.transform.DOScaleX(0.8f, animDuration).SetLoops(2, LoopType.Yoyo);
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        /*if (collision.gameObject.CompareTag("Platform"))
+        {
+            myRigidbody.transform.DOScaleX(1.2f, animDuration).SetLoops(2, LoopType.Yoyo);
+            myRigidbody.transform.DOScaleY(0.7f, animDuration).SetLoops(2, LoopType.Yoyo);
+            ResetScale();
+            
+        }*/
+
+        if (collision.gameObject.CompareTag("Platform")&& !_isLanding)
+        {
+            _isLanding = true;
+
+            myRigidbody.transform.DOKill();
+            Sequence squash = DOTween.Sequence();
+
+            squash.Append(myRigidbody.transform.DOScale(new Vector3(1.15f, 0.75f, 1), .17f).SetEase(ease));
+            squash.Append(myRigidbody.transform.DOScale(new Vector3(1f, 1.3f, 1), .17f).SetEase(ease));
+
+            squash.Append(myRigidbody.transform.DOScale(Vector3.one, animDuration));
+
+            squash.OnComplete(() =>
+            {
+                _isLanding = false;
+            });
         }
     }
 }
