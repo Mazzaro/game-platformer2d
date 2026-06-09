abc1d817f590d928e49b0d13558b9c91cf788a20
Mazzaro <mbg1@icomp.ufam.edu.br>
2026-06-09 15:13:07 -0400

    Add Player idle and run animations and fixing jumping bugs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UIElements;
+using System.ComponentModel;
 
 public class Player : MonoBehaviour
 {
@@ -19,6 +21,12 @@ public class Player : MonoBehaviour
     public float animDuration = .3f;
     public Ease ease = Ease.OutQuad;
 
+    [Header("Player Animation")]
+    public string boolRun = "Run";
+    public Animator animator;
+    public float playerSwipeDuration = .1f;
+    private float _facingDirection = 1f;
+
     private float _currentSpeed;
     private bool _isLanding;
 
@@ -29,69 +37,111 @@ public class Player : MonoBehaviour
         HandleMovement();
     }
 
+    void LateUpdate()
+    {
+        ApplyFacingDirection();
+    }
+
     public void HandleMovement()
     {
         
-        if(Input.GetKey(KeyCode.LeftControl))
+        if(Input.GetKey(KeyCode.LeftControl)){
             _currentSpeed = speedRun;
-        else
+            animator.speed = 1.5f;
+        }
+        else{
             _currentSpeed = speed;
+            animator.speed = 1.5f;
+        }
 
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            myRigidbody.velocity =  new Vector2(-_currentSpeed, myRigidbody.velocity.y);
+            SetFacingDirection(-1f);
+
+            myRigidbody.velocity = new Vector2(-_currentSpeed, myRigidbody.velocity.y);
+            animator.SetBool(boolRun, true);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            myRigidbody.velocity =  new Vector2(_currentSpeed, myRigidbody.velocity.y);
+            SetFacingDirection(1f);
+
+            myRigidbody.velocity = new Vector2(_currentSpeed, myRigidbody.velocity.y);
+            animator.SetBool(boolRun, true);
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+            animator.SetBool(boolRun, false);
         }
 
-        if (myRigidbody.velocity.x > 0)
+    }
+
+    private void SetFacingDirection(float direction)
+    {
+        _facingDirection = direction;
+        ApplyFacingDirection();
+    }
+
+    private void ApplyFacingDirection()
+    {
+        Transform playerTransform = myRigidbody.transform;
+
+        Vector3 currentScale = playerTransform.localScale;
+
+        float scaleX = Mathf.Abs(currentScale.x);
+
+        if (scaleX < 0.01f)
         {
-            myRigidbody.velocity += friction;
-        }
-        else if (myRigidbody.velocity.x < 0)
-        {
-            myRigidbody.velocity -= friction;
+            scaleX = 1f;
         }
 
+        currentScale.x = scaleX * _facingDirection;
+
+        playerTransform.localScale = currentScale;
     }
 
     public void HandleJump()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            myRigidbody.velocity =  Vector2.up * jumpForce;
-            myRigidbody.transform.localScale = Vector2.one;
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
 
-            DOTween.Kill(myRigidbody.transform);
-            HandleScaleJump();
+            Transform playerTransform = myRigidbody.transform;
+
+            DOTween.Kill(playerTransform);
+
+            playerTransform.localScale = new Vector3(_facingDirection, 1f, 1f);
+
+            HandleScaleJump(_facingDirection);
         }
     }
 
-    public void HandleScaleJump()
+    public void HandleScaleJump(float direction)
     {
-        myRigidbody.transform.DOScaleX(jumpScaleX, animDuration).SetLoops(2, LoopType.Yoyo);
-        myRigidbody.transform.DOScaleY(0.8f, animDuration).SetLoops(2, LoopType.Yoyo);
-        myRigidbody.transform.DOScaleY(jumpScaleY, animDuration).SetLoops(2, LoopType.Yoyo);
-        myRigidbody.transform.DOScaleX(0.8f, animDuration).SetLoops(2, LoopType.Yoyo);
-        
+        Transform playerTransform = myRigidbody.transform;
+        playerTransform.DOKill();
+
+        playerTransform.DOScaleX(direction * jumpScaleX, animDuration).SetLoops(2, LoopType.Yoyo);
+        playerTransform.DOScaleY(jumpScaleY, animDuration).SetLoops(2, LoopType.Yoyo);
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Platform")&& !_isLanding)
+        if (collision.gameObject.CompareTag("Platform") && !_isLanding)
         {
             _isLanding = true;
 
-            myRigidbody.transform.DOKill();
+            Transform playerTransform = myRigidbody.transform;
+            float direction = _facingDirection;
+
+            playerTransform.DOKill();
+
             Sequence squash = DOTween.Sequence();
 
-            squash.Append(myRigidbody.transform.DOScale(new Vector3(1.15f, 0.75f, 1), .17f).SetEase(ease));
-            squash.Append(myRigidbody.transform.DOScale(new Vector3(1f, 1.3f, 1), .17f).SetEase(ease));
-
-            squash.Append(myRigidbody.transform.DOScale(Vector3.one, animDuration));
+            squash.Append(playerTransform.DOScale(new Vector3(direction * 1.15f, 0.75f, 1), .17f).SetEase(ease));
+            squash.Append(playerTransform.DOScale(new Vector3(direction * 1f, 1.3f, 1), .17f).SetEase(ease));
+            squash.Append(playerTransform.DOScale(new Vector3(direction * 1f, 1f, 1), animDuration));
 
             squash.OnComplete(() =>
             {
